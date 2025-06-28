c8417cf78a671c2e4bd482a8d2a0b6452d00e228
Henok-Al <henok.gebresenbet@gmail.com>
2025-06-28 18:21:22 +0300

    Email Auth OTP

--- app/(auth)/login/_components/LoginForm.tsx
+++ app/(auth)/login/_components/LoginForm.tsx
@@ -11,36 +11,59 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { authClient } from "@/lib/auth-client";
-import { GithubIcon, Loader } from "lucide-react";
+import { GithubIcon, Loader, Loader2, Send } from "lucide-react";
 import Link from "next/link";
-import { useTransition } from "react";
+import { useRouter } from "next/navigation";
+import { useState, useTransition } from "react";
 import { toast } from "sonner";
 
-
 export default function LoginForm() {
-    const [githubPending, startGithubTransition] = useTransition(); //set for disabled state
-    
-      async function signInWithGithub() {
-        startGithubTransition(async () => {
-          await authClient.signIn.social({
-            provider: "github",
-            callbackURL: "/",
-            fetchOptions: {
-              onSuccess: () => {
-                toast.success("Signed in with Github, you will be redirected...");
-              },
-              onError: (error) => {
-                toast.error("Internal server error.");
-              },
-            },
-          });
-        });
-      }
+  const router = useRouter();
+  const [githubPending, startGithubTransition] = useTransition(); //set for disabled state
+  const [emailPending, startEmailTransition] = useTransition(); //set for disabled state
+  const [email, setEmail] = useState("");
+
+  async function signInWithGithub() {
+    startGithubTransition(async () => {
+      await authClient.signIn.social({
+        provider: "github",
+        callbackURL: "/",
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success("Signed in with Github, you will be redirected...");
+          },
+          onError: (error) => {
+            toast.error("Internal server error.");
+          },
+        },
+      });
+    });
+  }
+
+  function signInWithEmail() {
+    startEmailTransition(async () => {
+      await authClient.emailOtp.sendVerificationOtp({
+        email: email,
+        type: "sign-in", // or "email-verification", "forget-password",
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success("Email sent");
+            router.push(`/verify-request?email=${email}`);
+          },
+          onError: () => {
+            toast.error("Error sending email");
+          },
+        },
+      });
+    });
+  }
   return (
     <Card>
       <CardHeader>
         <CardTitle className="text-xl">Welcome Back!</CardTitle>
-        <CardDescription>Login with your Github or Email Account</CardDescription>
+        <CardDescription>
+          Login with your Github or Email Account
+        </CardDescription>
       </CardHeader>
 
       <CardContent className="flex flex-col gap-4">
@@ -71,10 +94,28 @@ export default function LoginForm() {
         <div className="grid gap-3">
           <div className="grid gap-2">
             <Label htmlFor="email">Email </Label>
-            <Input type="email" placeholder="h@example.co" />
+            <Input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              placeholder="h@example.co"
+              required
+            />
           </div>
 
-          <Button>Continue with Email</Button>
+          <Button onClick={signInWithEmail} disabled={emailPending}>
+            {emailPending ? (
+              <>
+                <Loader2 className="size-4 animate-spin" />
+                <span>Loading...</span>
+              </>
+            ) : (
+              <>
+                <Send className="size-4" />
+                <span> Continue with Email</span>
+              </>
+            )}
+          </Button>
         </div>
       </CardContent>
     </Card>
--- lib/auth.ts
+++ lib/auth.ts
@@ -2,15 +2,30 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./db";
 import { env } from "./env";
+import { emailOTP } from "better-auth/plugins";
+import { resend } from "./resend";
 
 export const auth = betterAuth({
-     database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
+  }),
+  socialProviders: {
+    github: {
+      clientId: env.AUTH_GITHUB_CLIENT_ID,
+      clientSecret: env.AUTH_GITHUB_SECRET,
+    },
+  },
+
+  plugins: [
+    emailOTP({
+      async sendVerificationOTP({ email, otp }) {
+        const { data, error } = await resend.emails.send({
+          from: "Prime LMS <onboarding@resend.dev>",
+          to: [email],
+          subject: "Prime LMS - Verify your email",
+          html: `Your OTP is <stong>${otp}</stong>`,
+        });
+      },
     }),
-    socialProviders: { 
-        github: { 
-           clientId: env.AUTH_GITHUB_CLIENT_ID, 
-           clientSecret: env.AUTH_GITHUB_SECRET, 
-        }, 
-    }, 
-})  
+  ],
+});
